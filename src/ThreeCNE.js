af3bfa5ca2cf1e43cfaa84607599f52c13f226a0
Scott Norvell <scottlnorvell@gmail.com>
2016-10-24 12:03:53 -0400

    Adding sweet new CNE build steps!

---
+++ src/ThreeCNE.js
@@ -0,0 +1,13 @@
+import './polyfills.js';
+
+export { LinearFilter, RGBFormat } from './constants.js';
+export { _Math as Math } from './math/Math.js';
+export { Mesh } from './objects/Mesh.js';
+export { MeshBasicMaterial } from './materials/MeshBasicMaterial';
+export { PerspectiveCamera } from './cameras/PerspectiveCamera.js';
+export { Scene } from './scenes/Scene.js';
+export { SphereGeometry } from './geometries/SphereGeometry';
+export { SphereBufferGeometry } from './geometries/SphereBufferGeometry';
+export { Texture } from './textures/Texture.js';
+export { Vector3 } from './math/Vector3.js';
+export { WebGLRenderer } from './renderers/WebGLRenderer.js';
