9fc1606335de13e59cfee3ef682af75f51398da8
Janet Lee <ekelak13@gmail.com>
2017-04-26 10:32:12 -0400

    fix omnitone reference to wav file, upgraded omnitone
    manually changed "var HRTFSET_URL = 'https://raw.githubusercontent.com/google/spatial-media/master/support/hrtfs/cube/';" to "var HRTFSET_URL = 'https://raw.githubusercontent.com/GoogleChrome/omnitone/master/src/resources';"

--- rollup.config.cne.js
+++ rollup.config.cne.js
@@ -33,18 +33,7 @@ export default {
 	indent: '\t',
 	plugins: [
 	  glsl(),
-	  nodeResolve({ jsnext: true, main: true }),
-	  commonjs({
-		  // non-CommonJS modules will be ignored, but you can also
-		  // specifically include/exclude files
-		  include: 'node_modules/**',  // Default: undefined
-
-		  // if true then uses of `global` won't be dealt with by this plugin
-		  ignoreGlobal: false,  // Default: false
-
-		  // if false then skip sourceMap generation for CommonJS modules
-		  sourceMap: false,  // Default: true
-	  })
+	  nodeResolve({ jsnext: true, main: true })
 	],
 	targets: [
 		{
